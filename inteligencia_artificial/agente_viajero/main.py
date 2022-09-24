ddfb4c70496cd605635bc90ec62f3d730f4dd78e
EF <efraisma.ef7@gmail.com>
2022-09-24 14:46:28 -0500

    Try modular structure

--- inteligencia_artificial/agente_viajero/main.py
+++ inteligencia_artificial/agente_viajero/main.py
@@ -1,10 +1,13 @@
-from random import sample, randint
-from pandas import DataFrame, concat
-from matplotlib.pyplot import subplots, show
+from .utils import BaseClass
+from .plot import PlotAgenteViajero
 
+from random import sample
+from pandas import DataFrame, concat
+
+from matplotlib.pyplot import show
 from IPython.display import clear_output
 
-class AgenteViajero:
+class AgenteViajero(BaseClass, PlotAgenteViajero):
     def __init__(self, cities_coordinates: list, population_size: int, n_generations: int) -> None:
         self.n_gen = n_generations
         self.pop_size = population_size
@@ -27,14 +30,6 @@ class AgenteViajero:
 
     def to_dataframe(self) -> None:
         self.df = DataFrame(self.population, columns=self.cit_name)
-
-
-    def calculate_distance(self, p: tuple, q: tuple) -> float:
-        sqt_diff = 0
-        for x, y in zip(p, q):
-            sqt_diff += (x - y)**2
-        dist = sqt_diff**0.5
-        return dist
 
     
     def aptitude_func(self, chromosone: list) -> float:
@@ -83,43 +78,6 @@ class AgenteViajero:
         return top_cities, top_dist
 
 
-    def inversion_reprod(self, prev_winner: list) -> list:
-        to_keep = randint(0,1)
-        if to_keep==0: parent = prev_winner[1:]
-        else: parent = prev_winner[:-1]
-
-        first_point = randint(0, len(parent) - 1)
-        end_point = randint(first_point + 1, len(parent))
-
-        fragment = parent[first_point:end_point]
-        inversion = fragment[::-1]
-
-        child = parent[:first_point] + inversion + parent[end_point:]
-
-        if to_keep==0: return prev_winner[:1] + child
-        else: return child + prev_winner[-1:]
-
-
-
-    def castling_reprod(self, parent: list) -> list:
-        first_point = randint(0, len(parent) - 2)
-
-        max_len_to_switch = len(parent[first_point:]) // 2
-        len_to_switch = randint(1, max_len_to_switch)        
-
-        first_point_end = first_point + len_to_switch
-
-        end_point = randint(first_point_end, len(parent) - len_to_switch)
-        end_point_end = end_point + len_to_switch
-
-        child = parent[:first_point] 
-        child += parent[end_point:end_point_end]
-        child += parent[first_point_end:end_point]
-        child += parent[first_point:first_point_end]
-        child += parent[end_point_end:]
-        return child
-
-
     def new_population(self, reprod_func: function) -> None:
         self.population = []
 
@@ -131,38 +89,6 @@ class AgenteViajero:
 
         self.to_dataframe()
         self.get_all_dist()
-
-
-    def plot_cities(self) -> None:
-        self.fig, self.axes = subplots(nrows=1, ncols=2, figsize=(10, 5))
-
-        self.cit_x, self.cit_y = [*zip(*self.cit_coor)]
-        self.axes[0].scatter(self.cit_x, self.cit_y)
-
-
-    def connectpoints(self, point: tuple, line_color: str, n_gen: int) -> None:
-        x1, x2 = self.cit_x[point[0]], self.cit_x[point[-1]]
-        y1, y2 = self.cit_y[point[0]], self.cit_y[point[-1]]
-
-        self.axes[0].plot([x1, x2],[y1, y2], c=line_color)
-        self.axes[0].set_title(f'Best route, gen #{n_gen}')
-
-    
-    def plot_route(self, route: list, **kwargs) -> None:
-        self.plot_cities()    
-        for city in route:
-            self.connectpoints(city, **kwargs)
-
-
-    def plot_distance(self, train_history: list) -> None:
-        self.axes[1].set_xlim([0,100])
-        self.axes[1].set_ylim([0, train_history[0][-1]*1.1])
-
-        acum_dist = [x[-1] for x in train_history]
-        self.axes[1].plot(acum_dist)
-
-        distance = f'Distance: {acum_dist[-1]:.2f}'
-        self.axes[1].set_title(distance)
 
 
     def train(self, tournaments_size: float, reprod_func: function, verbose: bool) -> None:
