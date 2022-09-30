fa039097dad8ece676a3fe82d1888e0819c42226
EF <efraisma.ef7@gmail.com>
2022-09-29 21:58:35 -0500

    Animated plot finished!

---
+++ inteligencia_artificial/tsp/plot.py
@@ -0,0 +1,58 @@
+from train import TSP
+
+from matplotlib.pyplot import subplots
+
+class PlotTSP(TSP):
+    def __init__(self, cities_coordinates: list, population_size: int, n_generations: int, tournament_size: float) -> None:
+        super().__init__(cities_coordinates, population_size, n_generations, tournament_size)
+        self.set_plot_env()
+
+
+    def plot_cities(self, **kwargs) -> None:
+        self.axes[0].scatter(self.cit_x, self.cit_y, **kwargs)
+        
+
+    def connectpoints(self, point: tuple, **kwargs) -> None:
+        x1, x2 = self.cit_x[point[0]], self.cit_x[point[-1]]
+        y1, y2 = self.cit_y[point[0]], self.cit_y[point[-1]]
+        self.axes[0].plot([x1, x2],[y1, y2], **kwargs)        
+
+
+    def plot_route(self, i: int, **kwargs) -> None:
+        self.axes[0].clear()
+
+        self.axes[0].set_title(f'Best route at gen #{i+1}')
+
+        self.plot_cities(**kwargs)
+
+        route = self.result['route'][i]
+        for city in route:
+            self.connectpoints(city, **kwargs)
+
+
+    def plot_distance(self, i: int, **kwargs) -> None:
+        top_dist = self.result['distance'][i]
+        distance = f'Distance: {top_dist:.2f}'
+
+        self.dist_x.append(i)
+        self.dist_y.append(top_dist)
+
+        self.axes[1].clear()
+
+        self.axes[1].set_title(distance)
+        self.axes[1].set_xlim([0, self.n_gen])
+        self.axes[1].set_ylim([0, self.result['distance'][0]])
+
+        self.axes[1].plot(self.dist_x, self.dist_y, **kwargs)
+
+
+    def set_plot_env(self) -> None:
+        self.fig, self.axes = subplots(nrows=1, ncols=2)
+        self.cit_x, self.cit_y = [*zip(*self.cit_coor)]
+
+        self.dist_x, self.dist_y = [], []
+
+
+    def plot_tsp(self, i, **kwargs) -> None:
+        self.plot_route(i, **kwargs)
+        self.plot_distance(i, **kwargs)
--- inteligencia_artificial/tsp/run.py
+++ inteligencia_artificial/tsp/run.py
@@ -1,51 +1,9 @@
-from train import TSP
+from plot import PlotTSP
 
-from matplotlib.pyplot import subplots, show
-from matplotlib.animation import ArtistAnimation
+from matplotlib.pyplot import show
+from matplotlib.animation import FuncAnimation
 
-
-
-class PlotTSP(TSP):
-    def __init__(self, cities_coordinates: list, population_size: int, n_generations: int, tournament_size: float) -> None:
-        super().__init__(cities_coordinates, population_size, n_generations, tournament_size)
-
-        self.train(reprod_functions=[self.inversion_reprod, self.castling_reprod])
-
-        self.fig, self.axes = subplots(nrows=1, ncols=2)
-        self.cit_x, self.cit_y = [*zip(*self.cit_coor)]
-        self.axes[1].set_xlim([0, self.n_gen])
-
-
-    def plot_cities(self, **kwargs) -> None:
-        self.axes[0].scatter(self.cit_x, self.cit_y, **kwargs)
-        
-
-    def connectpoints(self, point: tuple, **kwargs) -> None:
-        x1, x2 = self.cit_x[point[0]], self.cit_x[point[-1]]
-        y1, y2 = self.cit_y[point[0]], self.cit_y[point[-1]]
-        self.axes[0].plot([x1, x2],[y1, y2], **kwargs)
-
-
-    def plot_route(self, route: list, n_gen: int, **kwargs) -> None:
-        for city in route:
-            self.connectpoints(city, **kwargs)
-        self.axes[0].set_title(f'Best route, gen #{n_gen}')
-
-
-    def plot_distance(self, distance_history: list) -> None:
-        self.axes[1].plot(distance_history)
-
-        top_dist = list(distance_history)[-1]
-        distance = f'Distance: {top_dist:.2f}'
-        self.axes[1].set_title(distance)
-
-
-    def plot_tsp(self, i, **kwargs) -> None:
-        self.plot_cities(c='red')
-        self.plot_route(route=self.result['route'][i], n_gen=i+1, **kwargs)
-        self.plot_distance(distance_history=self.result['distance'][:i+1])
-
-
+# Coordenadas
 cities = [
     (1,3), (2,5), (2,7), (4,2), (4,4), 
     (4,7), (4,8), (5,3), (6,1), (6,6), 
@@ -53,11 +11,26 @@ cities = [
     (11,1), (11,4), (11,6), (12,7), (13,5),
 ]
 
-tsp = PlotTSP(cities_coordinates=cities, population_size=10, n_generations=10, tournament_size=0.5)
+# Parámetros
+tsp = PlotTSP(
+    cities_coordinates=cities,
+    population_size=100,
+    n_generations=100,
+    tournament_size=0.07,
+)
 
-ims = []
-for i in range(tsp.n_gen):
-    ims.append((tsp.plot_tsp(i, c='blue'),))
+# Entrenamiento
+tsp.train(reprod_functions=[
+    tsp.inversion_reprod, 
+    # tsp.castling_reprod,
+])
 
-im_ani = ArtistAnimation(tsp.fig, ims, interval=50, repeat_delay=3000, blit=True)
+# Gráfica
+anim = FuncAnimation(
+    tsp.fig, 
+    lambda x: tsp.plot_tsp(x, c='red'),
+    frames=tsp.n_gen,
+    interval=100,
+    repeat=False,
+)
 show()
