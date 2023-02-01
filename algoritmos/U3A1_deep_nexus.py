db8d9901b5fdb3d30588bbde9553fbe3cd086b2f
EF <efraisma.ef7@gmail.com>
2023-01-31 21:35:09 -0600

    Deep Search
    Recursively call deep search. Currently, works with full nexus. It must work correctly if there are other nodes not connected

--- algoritmos/U3A1_deep_nexus.py
+++ algoritmos/U3A1_deep_nexus.py
@@ -16,6 +16,7 @@ class DeepNexus:
         matches = search(pattern=patt, string=self.data, flags=IGNORECASE)
 
         self.nodes = eval(matches.group(1))
+        self.nodes = list(self.nodes)
         self.edges = eval(matches.group(2))
 
     def adjacency_matrix(self, is_symmetric: bool) -> None:
@@ -26,8 +27,19 @@ class DeepNexus:
             self.adjac[i, j] = 1
             if is_symmetric:
                 self.adjac[j, i] = 1
-
         print(self.adjac)
+
+    def get_adjancency_edges(self, node: int) -> list:
+        node_row = self.adjac[node, :].copy()
+        adjac_edges = filter(lambda x: node_row[x] == 1, range(self.n_nodes))
+        return list(adjac_edges)
+
+    def deep_search(self, node: int) -> None:
+        print(node)
+        self.nodes.pop(node)
+        for node_end in self.get_adjancency_edges(node):
+            if node_end in self.nodes:
+                self.deep_search(node_end)
 
 
 dn = DeepNexus()
@@ -35,3 +47,4 @@ dn = DeepNexus()
 dn.read_ugly_data(data_dir="algoritmos/U3A1_test.txt")
 dn.get_nodes_n_edges()
 dn.adjacency_matrix(is_symmetric=True)
+dn.deep_search(0)
