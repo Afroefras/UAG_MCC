82f0ad2138b9b717ee30352232c9e3dec62606ec
EF <efraisma.ef7@gmail.com>
2023-02-02 20:16:05 -0600

    Update U3A1_dijkstra.py
    Add new route distances to the last nodes' adjancency nodes

--- algoritmos/U3A1_dijkstra.py
+++ algoritmos/U3A1_dijkstra.py
@@ -9,35 +9,57 @@ class ShortestRoute:
         with open(data_dir, "r") as f:
             self.data = f.read()
 
-    def get_nodes_n_edges(self) -> None:
+    def get_vars(self) -> None:
         aux = r"[\S\s]*?"
         patt = rf"Nodes{aux}(\(*\d+[,\S\s]*?\)*)Edges{aux}(\[{aux}\])"
         matches = search(pattern=patt, string=self.data, flags=IGNORECASE)
 
-        self.nodes = eval(matches.group(1))
-        self.edges = eval(matches.group(2))
+        self.raw_nodes = eval(matches.group(1))
+        self.raw_edges = eval(matches.group(2))
 
     def structure_vars(self) -> None:
-        self.nodes = set(self.nodes)
+        self.nodes = set(self.raw_nodes)
 
-        self.edges_dist = {}
+        self.edges = {}
         for node in self.nodes:
-            adjac = {y: z["weight"] for x, y, z in self.edges if x == node}
-            self.edges_dist[node] = adjac
+            adjac = {}
+            for x, y, z in self.raw_edges:
+                if x == node:
+                    adjac[y] = z["weight"]
+            self.edges[(node,)] = adjac
 
-    def min_dist_adjac(self, node: int) -> tuple:
-        adjac = self.edges_dist[node]
+    def min_dist_adjac(self, node: tuple) -> tuple:
+        adjac = self.edges[node]
+        winner = min(adjac, key=adjac.get)
 
-        min_key = min(adjac, key=adjac.get)
-        min_dist = adjac[min_key]
+        return (winner,)
 
-        return min_key, min_dist
+    def add_route_distance(self, winner: tuple, route: tuple) -> None:
+        new_route = route + winner
+        self.edges[new_route] = self.edges[winner]
+
+        for i, _ in enumerate(new_route[:-1]):
+            current_node = new_route[i]
+            next_node = new_route[i + 1]
+
+            prev_dist = self.edges[(current_node,)][next_node]
+
+            dist_to_add = self.edges[new_route].items()
+            self.edges[new_route] = {x: y + prev_dist for x, y in dist_to_add}
+
+    def dijkstra(self) -> None:
+        pass
 
 
 sr = ShortestRoute()
 sr.read_ugly_data(data_dir="algoritmos/U3A1_Grafo_ponderado.txt")
-sr.get_nodes_n_edges()
+sr.get_vars()
 sr.structure_vars()
 
-print(sr.edges_dist)
-print(sr.min_dist_adjac(0))
+# a = sr.min_dist_adjac(node=(0,))
+# print(a)
+
+print(sr.edges)
+print("\n")
+sr.add_route_distance((5,), (0,))
+print(sr.edges)
