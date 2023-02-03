4a0c835bd270bbd08c231fece16dbbf4856776b6
EF <efraisma.ef7@gmail.com>
2023-02-02 19:05:47 -0600

    Update U3A1_dijkstra.py
    Structure distances in dict of dicts.
    
    Min distance of a node given its adjancency nodes

--- algoritmos/U3A1_dijkstra.py
+++ algoritmos/U3A1_dijkstra.py
@@ -1,4 +1,3 @@
-from numpy import zeros
 from re import search, IGNORECASE
 
 
@@ -12,38 +11,33 @@ class ShortestRoute:
 
     def get_nodes_n_edges(self) -> None:
         aux = r"[\S\s]*?"
-        patt = rf"Nodes{aux}(\d+[,\S\s]*?)Edges{aux}(\[{aux}\])"
+        patt = rf"Nodes{aux}(\(*\d+[,\S\s]*?\)*)Edges{aux}(\[{aux}\])"
         matches = search(pattern=patt, string=self.data, flags=IGNORECASE)
 
         self.nodes = eval(matches.group(1))
         self.edges = eval(matches.group(2))
 
+    def structure_vars(self) -> None:
         self.nodes = set(self.nodes)
-        self.edges = {(x[0], x[1]): x[2]["weight"] for x in self.edges}
 
-    def distance_matrix(self) -> None:
-        self.max_distance = max(self.edges.values())
+        self.edges_dist = {}
+        for node in self.nodes:
+            adjac = {y: z["weight"] for x, y, z in self.edges if x == node}
+            self.edges_dist[node] = adjac
 
-        self.edges_rev = {
-            x: -(y - self.max_distance - 1) for x, y in self.edges.items()
-        }
+    def min_dist_adjac(self, node: int) -> tuple:
+        adjac = self.edges_dist[node]
 
-        self.n_nodes = len(self.nodes)
-        self.dist = zeros((self.n_nodes, self.n_nodes))
+        min_key = min(adjac, key=adjac.get)
+        min_dist = adjac[min_key]
 
-        for i, j in self.edges_rev.keys():
-            self.dist[i, j] = self.edges_rev[(i, j)]
-
-    def get_adjancency_edges(self, node: int) -> set:
-        node_row = self.dist[node, :].copy()
-        adjac_nodes = filter(lambda x: node_row[x] > 0, range(self.n_nodes))
-        return set(adjac_nodes)
-
-        
+        return min_key, min_dist
 
 
 sr = ShortestRoute()
 sr.read_ugly_data(data_dir="algoritmos/U3A1_Grafo_ponderado.txt")
 sr.get_nodes_n_edges()
+sr.structure_vars()
 
-sr.distance_matrix()
+print(sr.edges_dist)
+print(sr.min_dist_adjac(0))
