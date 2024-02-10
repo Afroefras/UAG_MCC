dad042703f89275e92730a034d821dce34845318
EF <efraisma.ef7@gmail.com>
2024-02-10 12:17:43 -0600

    dynamic grid position

--- compu_cognitiva/grid.py
+++ compu_cognitiva/grid.py
@@ -1,36 +1,47 @@
 import pygame as pg
 
-TITLE = "Grid"
-TILES_HORIZONTAL = 10
-TILES_VERTICAL = 10
-TILE_SIZE = 80
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 800
-
-
 class Player:
-    def __init__(self, surface):
+    def __init__(self, surface, tile_size: int):
         self.surface = surface
-        self.pos = (40, 40)
+        self.tile_size = tile_size
+        # Posicionar la robotina en el centro de la primera cuadrícula
+        self.pos = (self.tile_size // 2, self.tile_size // 2)
 
     def draw(self):
-        pg.draw.circle(self.surface, (255, 255, 255), self.pos, 40)
+        # Dibujar la robotina como un círculo en su posición actual
+        pg.draw.circle(self.surface, (255, 255, 255), self.pos, self.tile_size // 2)
 
     def move(self, target):
-        x = (80 * (target[0] // 80)) + 40
-        y = (80 * (target[1] // 80)) + 40
+        # Calcular la nueva posición basada en el tamaño de la cuadrícula y el clic objetivo
+        x = (self.tile_size * (target[0] // self.tile_size)) + (self.tile_size // 2)
+        y = (self.tile_size * (target[1] // self.tile_size)) + (self.tile_size // 2)
 
+        # Actualizar la posición de la robotina
         self.pos = (x, y)
 
 
 class Game:
-    def __init__(self):
+    def __init__(
+            self,
+            title: str,
+            tile_size: int,
+            tiles_horizontal: int,
+            tiles_vertical: int,
+        ):
         pg.init()
         self.clock = pg.time.Clock()
-        pg.display.set_caption(TITLE)
-        self.surface = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pg.display.set_caption(title)
+
+        self.tile_size = tile_size
+        self.tiles_horizontal = tiles_horizontal
+        self.tiles_vertical = tiles_vertical
+
+        window_width = self.tile_size * self.tiles_horizontal
+        window_height = self.tile_size * self.tiles_vertical
+        self.surface = pg.display.set_mode((window_width, window_height))
+
         self.loop = True
-        self.player = Player(self.surface)
+        self.player = Player(self.surface, self.tile_size)
 
     def main(self):
         while self.loop:
@@ -39,12 +50,12 @@ class Game:
 
     def grid_loop(self):
         self.surface.fill((0, 0, 0))
-        for row in range(TILES_HORIZONTAL):
-            for col in range(row % 2, TILES_HORIZONTAL, 2):
+        for row in range(self.tiles_horizontal):
+            for col in range(row % 2, self.tiles_horizontal, 2):
                 pg.draw.rect(
                     self.surface,
                     (40, 40, 40),
-                    (row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE),
+                    (row * self.tile_size, col * self.tile_size, self.tile_size, self.tile_size),
                 )
         self.player.draw()
         for event in pg.event.get():
@@ -60,5 +71,10 @@ class Game:
 
 
 if __name__ == "__main__":
-    mygame = Game()
+    mygame = Game(
+        title="Robotina",
+        tile_size=35, # Tamaño de cada cuadrícula
+        tiles_horizontal=30,
+        tiles_vertical=20
+    )
     mygame.main()
